1b2108ea62743d4756caa950fb614ed83ced863b
yahui <xlp604@163.com>
2016-08-12 18:57:21 +0800

    添加 ArticleListTableViewCell

---
+++ RssReader/ArticleListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleListTableViewCell.swift
+//  RssReader
+//
+//  Created by Yahui Duan on 16/8/12.
+//  Copyright © 2016年 Yahui.Duan. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
